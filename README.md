cde9743642ac516aaf4a5a066f641ed9b185e4bf
Madkour17 <madkour17@hotmail.com>
2022-08-19 17:19:25 +0200

    add another solution for two-sum problem

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # problem-solving
 
-
 ### [1- palindrome checker](https://github.com/Madkour17/problem-solving/blob/main/palindrome-checker.js)
+
 ### [2- Contains Duplicate](https://github.com/Madkour17/problem-solving/blob/main/contains-duplicate.js)
-### [3- Two Sum](https://github.com/Madkour17/problem-solving/blob/main/two-sum.js)
+
+### [3- Two Sum ( 2 solutions )](https://github.com/Madkour17/problem-solving/blob/main/two-sum.js)
+
 ### [4- Reverse String](https://github.com/Madkour17/problem-solving/blob/main/reverse-string.js)
+
 ### [5- Factorial](https://github.com/Madkour17/problem-solving/blob/main/factorial.js)
+
 ### [6- Longest Word](https://github.com/Madkour17/problem-solving/blob/main/longest-word.js)
+
 ### [7- Largest Nums](https://github.com/Madkour17/problem-solving/blob/main/largest-nums.js)
--- two-sum.js
+++ two-sum.js
@@ -1,10 +1,23 @@
-function twoSum(nums, target) {
-    for (let x = 0; x < nums.length; x++) {
-        for (y = 0; y < nums.length; y++) {
-            if (x !== y && (nums[x] + nums[y] === target)) {
-                return [x, y]
-            }
-        }
+// 1st method using 2 for loops O(n^2) Time
+function twoSumUsing2ForLoops(nums, target) {
+  for (let x = 0; x < nums.length; x++) {
+    for (y = 0; y < nums.length; y++) {
+      if (x !== y && nums[x] + nums[y] === target) {
+        return [x, y];
+      }
     }
-};
+  }
+  return -1;
+}
 
+//2nd method using hash table O(n) Time
+function twoSumUsingHashTable(nums, target) {
+  const obj = {};
+  for (let x = 0; x < nums.length; x++) {
+    if (obj[target - nums[x]]) {
+      return [x, obj[target - nums[x]]];
+    }
+    obj[nums[x]] = x;
+  }
+  return -1;
+}
